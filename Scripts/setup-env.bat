e9be8964f0a4e64bcc79fa7120ac060d872c1d2e
Dorian Pinaud <dorian.pinaud@gmail.com>
2022-04-11 22:38:10 +0200

    Fix installation setup

--- Scripts/setup-env.bat
+++ Scripts/setup-env.bat
@@ -2,16 +2,19 @@
 @echo off
 :: Stepup the anaconda env
 set path=%path%;%userprofile%\anaconda3\Scripts\
-call conda env list | findstr /R "^cv-nd " > nul
+conda env list | findstr /R "^cv-nd " > nul
 
 :: If virtual env conda not exist create it
 if errorlevel 1 (
     echo "Install environement"
     echo off
-    conda create --name cv-nd python=3.6
-    call activate cv-nd
+    call activate
+    conda install python=3.6
     conda install pytorch-cpu -c pytorch
-
+    conda create --name cv-nd python=3.6
+    echo "Environment activated"
+    call activate cv-nd
+    
     :: Install python dependencies in the current virtual env
     pip install -r .\Scripts\requirements.txt
     
--- readme.md
+++ readme.md
@@ -2,6 +2,10 @@
 
 This project keeps track on my progress on [the computer vision udacity nanodegree](https://learn.udacity.com/nanodegrees/nd891/parts/cd1859/). Feel free to explore it !
 
+## Requirement
+
+Anaconda 3 must be installed in the user profile.
+
 ## Setup
 
 The script [setup-env.bat](.\setup-env.bat) create the conda virtual environment and install the dependencies. If it is already created and installed, the script just activate the conda virtual environment.
